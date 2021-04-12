ebacb90f462898e00fcc2461f1999de5b33d6dd4
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-04-12 14:10:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function ans1(){
+    window.alert("They are named after colours. Colorful town names include Lavender Town, Cerulean City, and Viridian City. The player's hometown, Pallet Town, also fits in with this theme.")
+  };
+  function ans2(){
+    window.alert("Lapras. This transport Pokemon is known for carrying travelers on its back as it swims. In the anime, the protagonists use Lapras to travel to new regions.")
+  };
+  function ans3(){
+    window.alert("Mr. Mime. Only players who are in Europe can catch Mr. Mime. Other region-specific Pokémon include Taurus in the US/Canada and Farfetch'd in Eastern Asia.")
+  }
+  function ans4(){
+    window.alert("They are all baby Pokemon. Starting with the second generation of Pokémon, babies were introduced. These Pokémon cannot be bred and can evolve into more powerful forms.")
+  };
+  function ans5(){
+    window.alert("Ninetales. According to some legends, a kitsune can have up to nine tails that signify its power and wisdom. Ninetales echos this legend with its nine fluffy tails.")
+  };
+  function ans6(){
+    window.alert("1996. The first two Pokémon games, Red and Green, debuted in Japan on February 27, 1996. The series came to the North American market on September 28, 1998 and was released in Europe over a year after that.")
+  };
+  function ans7(){
+    window.alert("Item duplication. Following a specific series of steps in the Generation I games causes an encounter with MissingNo. This duplicates whatever item a player put in their 6th inventory slot, often a coveted item like Rare Candy.")
+  };
+  function ans8(){
+    window.alert("Mew. The code for Mew is actually present in the Generation I games, and this Pokemon is obtainable through glitches or cheats. The rumor about Mew hiding under a truck near the S.S. Anne, however, has proven to be untrue.")
+  };
+  function ans9(){
+    window.alert("Raises a Pokemon's level. Using a Rare Candy on a Pokémon will instantly raise its level by 1. These items are usually in limited supply, so they're considered rather valuable by players.")
+  };
+  function ans10(){
+    window.alert("Game Boy. The debut games were created for Nintendo's handheld system, the Game Boy. Future Pokémon games would also be on Nintendo handheld devices, such as the Game Boy Advance and Nintendo DS.")
+  }
+  
+  
+  
